8881d7de750ebdfc102ea90f82fc923d907c445f
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-05 11:32:30 +0100

    Evolution & amelioration de l'api auth : gestion de la creation du token, de son check, de son rafraichissement.
    Peut-être : Prise en charge de la methode DELETE pour supprimer le token

--- www/public/api/auth/index.php
+++ www/public/api/auth/index.php
@@ -6,29 +6,48 @@ header('Access-Control-Allow-Origin: *');
 
 header('Content-Type: application/json');
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(isset($_POST["token"])){
-        $token = $_POST["token"];
-        //vérification
-    }else if(isset($_POST["email"]) && isset($_POST["password"])){
-        //check login credential
+http_response_code(200);
 
-    }
-}
+$data = json_decode(file_get_contents("php://input"), true);
 
+//var_dump($data);
 
-
-header("Content-type: application/json");
-
+//send only data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $credits = json_decode(file_get_contents('php://input'), true);
-    if (isset($credits["login"]) && isset($credits["password"])) {
-        $headers = array("alg" => "HS256", "typ" => "JWT");
-        $payload = array("login" => $credits["login"], "password" => $credits["password"], "exp" => time() + 3600);
-        $response = array("status_message" => "OK", "status_code" => 200, "token" => generate_jwt($headers, $payload, 4500));
-        echo json_encode($response);
+    //login and password defined => creating the token
+    if (isset($data["login"]) && isset($data["password"])) {
+        //check validity/truth of the login credentials
+
+        //generate the response and so the token
+        $response = array("status_message" => "OK", "status_code" => 200, "token" => encode($data["login"]));
     }
-} elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $response = array("status_message" => "Unsupported method", "status_code" => 400, "token" => "");
-    echo json_encode($response);
+    //the case to check the token authenticity
+    elseif(isset($data["token"])){
+        //valid or not
+        if(is_valid_token($data["token"])){
+            $response = array("status_message" => "OK", "status_code" => 200,"valid"=> true);
+        }else{
+            http_response_code(400);
+            $response = array("status_message" => "Token is invalid", "status_code" => 400,"valid"=> false);
+        }
+    }
+    //default case
+    else {
+        http_response_code(400);
+        $response = array("status_message" => "Please provide a token", "status_code" => 400);
+    }
 }
+//update the token (refresh exp time)
+elseif($_SERVER["REQUEST_METHOD"] == "PATCH") {
+    //if valid, refreshing token
+    if(is_valid_token($data["token"])){
+        $response = array("status_message" => "OK", "status_code" => 200, "token" => refreshJwt($data["token"]));
+    }else{
+        http_response_code(400);
+        $response = array("status_message" => "Token is invalid", "status_code" => 400);
+    }
+}
+else{
+    $response = array("status_message" => "Unsupported method", "status_code" => 400, "token" => "");
+}
+echo json_encode($response);
