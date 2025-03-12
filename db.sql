cf50a8ba1d867ac55839de8fb2b29d84dd95b10e
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-12 15:01:02 +0100

    modification de la bd + ajout d'échantillons afin de regénérer facilement les données

--- db.sql
+++ db.sql
@@ -4,18 +4,18 @@ CREATE DATABASE IF NOT EXISTS messup;
 
 USE messup;
 
+CREATE TABLE users (
+    idUser int PRIMARY KEY AUTO_INCREMENT,
+    picture BINARY,
+    username VARCHAR(50),
+    password VARCHAR(60)
+);
+
 CREATE TABLE message (
     idMessage int PRIMARY KEY AUTO_INCREMENT,
     content VARCHAR(200) NOT NULL,
     time TIMESTAMP,
     idUser int,
     idSalle int NOT NULL,
-    CONSTRAINT fk_idUser FOREIGN KEY (idUser) REFERENCES users(idUser)
+    CONSTRAINT fk_idUser FOREIGN KEY (idUser) REFERENCES users(idUser)#, CONSTRAINT fk_idSalle FOREIGN KEY (idSalle) REFERENCES Salle(idSalle)
 );
-
-CREATE TABLE users (
-    idUser int PRIMARY KEY AUTO_INCREMENT,
-    picture BINARY,
-    username VARCHAR(50),
-    password VARCHAR(60)
-)
