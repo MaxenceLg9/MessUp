3d8601f693fd9addc648d550a52b3571fff1d6ec
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-12 15:09:19 +0100

    changement des noms pour match les maj

--- www/public/api/messages/enregistrer.php
+++ www/public/api/messages/enregistrer.php
@@ -1,15 +1,20 @@
 <?php
 
 require_once "{$_SERVER["DOCUMENT_ROOT"]}/../libs/modele/Message.php";
+require_once "{$_SERVER["DOCUMENT_ROOT"]}/../libs/modele/Token.php";
 
 use function Message\nouveauMessage;
 
 header('Content-Type: application/json');
 
+if(!apiVerifyToken()){
+
+}
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $jsonBody = json_decode(file_get_contents('php://input'), true);
-    if(isset($jsonBody["message"]) && isset($jsonBody["salle"]) && isset($jsonBody["author"])) {
-        $data = nouveauMessage($jsonBody["message"], $jsonBody["salle"], $jsonBody["author"]);
+    if(isset($jsonBody["message"]) && isset($jsonBody["idSalle"]) && isset($jsonBody["idUser"])) {
+        $data = nouveauMessage($jsonBody["message"], $jsonBody["idSalle"], $jsonBody["idUser"]);
         $message = array("status" => 200, "response" => "Message bien ajouté", "data" => $data);
     }
     else {
