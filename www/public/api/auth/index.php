853ad2420f791c17dd0cc1db361d010e36f4e0d7
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-15 00:15:06 +0100

    Ajout de la méthode d'inscription en vérifiant si le champ confirmpassword est initialisé. Déroulement normal sur le reste

--- www/public/api/auth/index.php
+++ www/public/api/auth/index.php
@@ -4,11 +4,11 @@ require_once "{$_SERVER["DOCUMENT_ROOT"]}/../libs/token/jwt_utils.php";
 require_once "{$_SERVER["DOCUMENT_ROOT"]}/../libs/modele/Users.php";
 
 use function Users\checkUser;
+use function Users\newUser;
+use function Users\getUserByUsername;
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
-http_response_code(200);
 
 $jsonBody = json_decode(file_get_contents("php://input"), true);
 
@@ -16,15 +16,34 @@ $jsonBody = json_decode(file_get_contents("php://input"), true);
 //send only data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //login and password defined => creating the token
-    if (isset($jsonBody["login"]) && isset($jsonBody["password"])) {
-        //check validity/truth of the login credentials
-        $user = checkUser($jsonBody["login"], $jsonBody["password"]);
-        if(empty($user)){
-            http_response_code(400);
-            $response = array("response" => "Invalid login or password", "status" => 400);
-        }else {
-            //generate the response and so the token
-            $response = array("response" => "OK", "status" => 200, "token" => encode($user["username"],$user["idUser"]));
+    if (isset($jsonBody["username"]) && isset($jsonBody["password"])) {
+        //create user
+        if(isset($jsonBody["confirmpassword"])){
+            if($jsonBody["password"] != $jsonBody["confirmpassword"]){
+                $response = array("response" => "Passwords do not match", "status" => 400);
+            }
+            else{
+                $user = getUserByUsername($jsonBody["username"]);
+                if (!empty($user)) {
+                    $response = array("response" => "Username déjà pris", "status" => 400);
+                } else if(empty(newUser($jsonBody["username"], $jsonBody["password"]))){
+
+                    $response = array("response" => "Erreur lors de la création de l'utilisateur", "status" => 400);
+                } else {
+                    $response = array("response" => "OK", "status" => 200);
+                }
+            }
+        }
+        else {
+            //check validity/truth of the login credentials
+            $user = checkUser($jsonBody["username"], $jsonBody["password"]);
+            if (empty($user)) {
+                $response = array("response" => "Invalid login or password", "status" => 400);
+            } else {
+                //generate the response and so the token
+                $response = array("response" => "OK", "status" => 200, "token" => encode($user["username"], $user["idUser"]));
+                setcookie("token", $response["token"], time() + 900, "/");
+            }
         }
     }
     //the case to check the token authenticity
@@ -33,13 +52,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if(is_valid_token($jsonBody["token"])){
             $response = array("response" => "OK", "status" => 200,"valid"=> true);
         }else{
-            http_response_code(400);
             $response = array("response" => "Token is invalid", "status" => 400,"valid"=> false);
         }
     }
     //default case
     else {
-        http_response_code(400);
         $response = array("response" => "Please provide a token", "status" => 400);
     }
 }
@@ -49,11 +66,11 @@ elseif($_SERVER["REQUEST_METHOD"] == "PUT") {
     if(is_valid_token($jsonBody["token"])){
         $response = array("response" => "OK", "status" => 200, "token" => refreshJwt($jsonBody["token"]));
     }else{
-        http_response_code(405);
         $response = array("response" => "Token is invalid", "status" => 405);
     }
 }
 else{
     $response = array("response" => "Unsupported method", "status" => 400, "token" => "");
 }
+http_response_code($response["status"]);
 echo json_encode($response);
