e7e1f90c9a786f181ca3f49a399db967b5105171
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-03-29 15:36:48 +0100

    changement de la connexion à la bd

--- www/libs/db/connexion.php
+++ www/libs/db/connexion.php
@@ -1,6 +1,6 @@
 <?php
 
 function creerConnexion() : PDO {
-    return new PDO('mysql:host=localhost;dbname=messup;charset=utf8','root','pq04WX11me2005!');
-//    return new PDO('mysql:host=db;dbname=messup;charset=utf8','root','pq04WX11me2005!');
+    return new PDO('mysql:host=localhost;dbname=messup;charset=utf8','root','pq04WX11me2005!'); //machine
+//    return new PDO('mysql:host=db;dbname=messup;charset=utf8','root','pq04WX11me2005!'); //container docker
 }
