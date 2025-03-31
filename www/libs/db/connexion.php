00fbc22c2b902ef180f0b0965388c5a1474c5ad8
Maxence <116764898+MaxenceLg9@users.noreply.github.com>
2025-03-31 09:00:27 +0200

    Delete www/libs/db directory

--- www/libs/db/connexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function creerConnexion() : PDO {
-    return new PDO('mysql:host=mysql-maxencelg.alwaysdata.net;dbname=maxencelg_messup;charset=utf8','maxencelg_user','$iutinfo'); //machine
-//    return new PDO('mysql:host=db;dbname=messup;charset=utf8','root','pq04WX11me2005!'); //container docker
-}
